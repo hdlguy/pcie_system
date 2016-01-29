cb77d075e1ca34300f78af18d1146de16beb3d33
pedro <hdlguy@hotmail.com>
2016-01-29 08:11:17 -0700

    adding spi ram for testing

--- source/system.tcl
+++ source/system.tcl
@@ -10,7 +10,7 @@
 ################################################################
 # Check if script is running in correct Vivado version.
 ################################################################
-set scripts_vivado_version 2014.4
+set scripts_vivado_version 2015.4
 set current_vivado_version [version -short]
 
 if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
@@ -32,6 +32,13 @@ if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
 #    create_project project_1 myproj -part xc7vx690tffg1761-2
 #    set_property BOARD_PART xilinx.com:vc709:part0:1.5 [current_project]
 
+# CHECKING IF PROJECT EXISTS
+if { [get_projects -quiet] eq "" } {
+   puts "ERROR: Please open or create a project!"
+   return 1
+}
+
+
 
 # CHANGE DESIGN NAME HERE
 set design_name system
@@ -39,13 +46,6 @@ set design_name system
 # If you do not already have an existing IP Integrator design open,
 # you can create a design using the following command:
 #    create_bd_design $design_name
-
-# CHECKING IF PROJECT EXISTS
-if { [get_projects -quiet] eq "" } {
-   puts "ERROR: Please open or create a project!"
-   return 1
-}
-
 
 # Creating design if needed
 set errMsg ""
@@ -159,28 +159,51 @@ proc create_hier_cell_pcie_bridge { parentCell nameHier } {
   create_bd_pin -dir I -type rst sys_rst_n
 
   # Create instance: axi_pcie3_0, and set properties
-  set axi_pcie3_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_pcie3:1.0 axi_pcie3_0 ]
-  set_property -dict [ list CONFIG.PCIE_BOARD_INTERFACE {pcie_express} CONFIG.SYS_RST_N_BOARD_INTERFACE {pcie_perst} CONFIG.axi_data_width {128_bit} CONFIG.axisten_freq {125} CONFIG.pf0_bar0_scale {Megabytes} CONFIG.pf0_bar0_size {16} CONFIG.pf0_device_id {7024} CONFIG.pl_link_cap_max_link_speed {5.0_GT/s} CONFIG.pl_link_cap_max_link_width {X4} CONFIG.plltype {QPLL1}  ] $axi_pcie3_0
+  set axi_pcie3_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_pcie3:2.0 axi_pcie3_0 ]
+  set_property -dict [ list \
+CONFIG.PCIE_BOARD_INTERFACE {pcie_express} \
+CONFIG.SYS_RST_N_BOARD_INTERFACE {pcie_perst} \
+CONFIG.axi_data_width {64_bit} \
+CONFIG.axisten_freq {125} \
+CONFIG.pf0_bar0_scale {Megabytes} \
+CONFIG.pf0_bar0_size {16} \
+CONFIG.pf0_device_id {7024} \
+CONFIG.pl_link_cap_max_link_speed {5.0_GT/s} \
+CONFIG.pl_link_cap_max_link_width {X1} \
+CONFIG.plltype {QPLL1} \
+ ] $axi_pcie3_0
 
   # Create instance: axi_pcie3_0_axi_periph, and set properties
   set axi_pcie3_0_axi_periph [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_pcie3_0_axi_periph ]
-  set_property -dict [ list CONFIG.NUM_MI {1}  ] $axi_pcie3_0_axi_periph
+  set_property -dict [ list \
+CONFIG.NUM_MI {1} \
+ ] $axi_pcie3_0_axi_periph
 
   # Create instance: fit_timer_0, and set properties
   set fit_timer_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:fit_timer:2.0 fit_timer_0 ]
-  set_property -dict [ list CONFIG.C_NO_CLOCKS {62500000}  ] $fit_timer_0
+  set_property -dict [ list \
+CONFIG.C_NO_CLOCKS {62500000} \
+ ] $fit_timer_0
 
   # Create instance: proc_sys_reset_0, and set properties
   set proc_sys_reset_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 proc_sys_reset_0 ]
-  set_property -dict [ list CONFIG.C_AUX_RESET_HIGH {0} CONFIG.USE_BOARD_FLOW {true}  ] $proc_sys_reset_0
+  set_property -dict [ list \
+CONFIG.C_AUX_RESET_HIGH {0} \
+CONFIG.USE_BOARD_FLOW {true} \
+ ] $proc_sys_reset_0
 
   # Create instance: util_ds_buf_0, and set properties
-  set util_ds_buf_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:util_ds_buf:2.0 util_ds_buf_0 ]
-  set_property -dict [ list CONFIG.C_BUF_TYPE {IBUFDSGTE}  ] $util_ds_buf_0
+  set util_ds_buf_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:util_ds_buf:2.1 util_ds_buf_0 ]
+  set_property -dict [ list \
+CONFIG.C_BUF_TYPE {IBUFDSGTE} \
+ ] $util_ds_buf_0
 
   # Create instance: xlconstant_0, and set properties
   set xlconstant_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 xlconstant_0 ]
-  set_property -dict [ list CONFIG.CONST_VAL {19} CONFIG.CONST_WIDTH {5}  ] $xlconstant_0
+  set_property -dict [ list \
+CONFIG.CONST_VAL {19} \
+CONFIG.CONST_WIDTH {5} \
+ ] $xlconstant_0
 
   # Create interface connections
   connect_bd_intf_net -intf_net axi_pcie3_0_M_AXI [get_bd_intf_pins axi_pcie3_0/M_AXI] [get_bd_intf_pins axi_pcie3_0_axi_periph/S00_AXI]
@@ -199,7 +222,7 @@ proc create_hier_cell_pcie_bridge { parentCell nameHier } {
   connect_bd_net -net reset_rtl_1 [get_bd_pins ext_reset_in] [get_bd_pins proc_sys_reset_0/ext_reset_in]
   connect_bd_net -net util_ds_buf_0_IBUF_OUT [get_bd_pins axi_pcie3_0/refclk] [get_bd_pins util_ds_buf_0/IBUF_OUT]
   connect_bd_net -net xlconstant_0_dout [get_bd_pins axi_pcie3_0/msi_vector_num] [get_bd_pins xlconstant_0/dout]
-  
+
   # Restore current instance
   current_bd_instance $oldCurInst
 }
@@ -241,9 +264,13 @@ proc create_root_design { parentCell } {
   set axi_clk [ create_bd_port -dir O axi_clk ]
   set axi_reset_n [ create_bd_port -dir O -from 0 -to 0 axi_reset_n ]
   set cpu_reset [ create_bd_port -dir I -type rst cpu_reset ]
-  set_property -dict [ list CONFIG.POLARITY {ACTIVE_HIGH}  ] $cpu_reset
+  set_property -dict [ list \
+CONFIG.POLARITY {ACTIVE_HIGH} \
+ ] $cpu_reset
   set pcie_perst [ create_bd_port -dir I -type rst pcie_perst ]
-  set_property -dict [ list CONFIG.POLARITY {ACTIVE_LOW}  ] $pcie_perst
+  set_property -dict [ list \
+CONFIG.POLARITY {ACTIVE_LOW} \
+ ] $pcie_perst
   set pcie_perstn [ create_bd_port -dir I -type rst pcie_perstn ]
   set pcie_refclk_N [ create_bd_port -dir I pcie_refclk_N ]
   set pcie_refclk_P [ create_bd_port -dir I pcie_refclk_P ]
@@ -333,7 +360,97 @@ proc create_root_design { parentCell } {
 
   # Create address segments
   create_bd_addr_seg -range 0x10000 -offset 0x44A00000 [get_bd_addr_spaces pcie_bridge/axi_pcie3_0/M_AXI] [get_bd_addr_segs regfilex16_v1_0_0/s00_axi/reg0] SEG_regfilex16_v1_0_0_reg0
-  
+
+  # Perform GUI Layout
+  regenerate_bd_layout -layout_string {
+   guistr: "# # String gsaved with Nlview 6.5.5  2015-06-26 bk=1.3371 VDI=38 GEI=35 GUI=JA:1.8
+#  -string -flagsOSRD
+preplace port cpu_reset -pg 1 -y 110 -defaultsOSRD
+preplace port pcie_refclk_N -pg 1 -y 50 -defaultsOSRD
+preplace port pcie_refclk_P -pg 1 -y 70 -defaultsOSRD
+preplace port pcie_perst -pg 1 -y 130 -defaultsOSRD
+preplace port axi_clk -pg 1 -y 120 -defaultsOSRD
+preplace port pcie_perstn -pg 1 -y 90 -defaultsOSRD
+preplace port pcie_7x_mgt -pg 1 -y 80 -defaultsOSRD
+preplace portBus slv_read11 -pg 1 -y 400 -defaultsOSRD
+preplace portBus slv_reg8 -pg 1 -y 350 -defaultsOSRD
+preplace portBus slv_read12 -pg 1 -y 420 -defaultsOSRD
+preplace portBus slv_reg9 -pg 1 -y 370 -defaultsOSRD
+preplace portBus slv_read13 -pg 1 -y 440 -defaultsOSRD
+preplace portBus slv_read14 -pg 1 -y 460 -defaultsOSRD
+preplace portBus slv_read0 -pg 1 -y 180 -defaultsOSRD
+preplace portBus slv_read1 -pg 1 -y 200 -defaultsOSRD
+preplace portBus slv_read15 -pg 1 -y 480 -defaultsOSRD
+preplace portBus slv_read2 -pg 1 -y 220 -defaultsOSRD
+preplace portBus slv_reg0 -pg 1 -y 190 -defaultsOSRD
+preplace portBus slv_read3 -pg 1 -y 240 -defaultsOSRD
+preplace portBus slv_reg1 -pg 1 -y 210 -defaultsOSRD
+preplace portBus slv_read4 -pg 1 -y 260 -defaultsOSRD
+preplace portBus slv_reg10 -pg 1 -y 390 -defaultsOSRD
+preplace portBus slv_reg2 -pg 1 -y 230 -defaultsOSRD
+preplace portBus slv_read5 -pg 1 -y 280 -defaultsOSRD
+preplace portBus slv_reg11 -pg 1 -y 410 -defaultsOSRD
+preplace portBus slv_reg3 -pg 1 -y 250 -defaultsOSRD
+preplace portBus slv_read6 -pg 1 -y 300 -defaultsOSRD
+preplace portBus slv_reg12 -pg 1 -y 430 -defaultsOSRD
+preplace portBus slv_reg4 -pg 1 -y 270 -defaultsOSRD
+preplace portBus axi_reset_n -pg 1 -y 100 -defaultsOSRD
+preplace portBus slv_read7 -pg 1 -y 320 -defaultsOSRD
+preplace portBus slv_reg13 -pg 1 -y 450 -defaultsOSRD
+preplace portBus slv_reg5 -pg 1 -y 290 -defaultsOSRD
+preplace portBus slv_read8 -pg 1 -y 340 -defaultsOSRD
+preplace portBus slv_reg14 -pg 1 -y 470 -defaultsOSRD
+preplace portBus slv_reg6 -pg 1 -y 310 -defaultsOSRD
+preplace portBus slv_read9 -pg 1 -y 360 -defaultsOSRD
+preplace portBus slv_read10 -pg 1 -y 380 -defaultsOSRD
+preplace portBus slv_reg15 -pg 1 -y 490 -defaultsOSRD
+preplace portBus slv_reg7 -pg 1 -y 330 -defaultsOSRD
+preplace inst pcie_bridge -pg 1 -lvl 1 -y 90 -defaultsOSRD
+preplace inst regfilex16_v1_0_0 -pg 1 -lvl 2 -y 340 -defaultsOSRD
+preplace netloc pcie_bridge_pcie_7x_mgt 1 1 2 NJ 80 NJ
+preplace netloc slv_read6_1 1 0 2 NJ 300 NJ
+preplace netloc slv_read5_1 1 0 2 NJ 280 NJ
+preplace netloc slv_read4_1 1 0 2 NJ 260 NJ
+preplace netloc slv_read11_1 1 0 2 NJ 400 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg8 1 2 1 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg9 1 2 1 NJ
+preplace netloc slv_read2_1 1 0 2 NJ 220 NJ
+preplace netloc slv_read15_1 1 0 2 NJ 480 NJ
+preplace netloc slv_read8_1 1 0 2 NJ 340 NJ
+preplace netloc pcie_perst_1 1 0 1 NJ
+preplace netloc axi_pcie3_0_axi_periph_M00_AXI 1 1 1 290
+preplace netloc regfilex16_v1_0_0_slv_reg0 1 2 1 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg10 1 2 1 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg1 1 2 1 NJ
+preplace netloc slv_read3_1 1 0 2 NJ 240 NJ
+preplace netloc slv_read10_1 1 0 2 NJ 380 NJ
+preplace netloc slv_read0_1 1 0 2 NJ 180 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg11 1 2 1 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg2 1 2 1 NJ
+preplace netloc IBUF_DS_N_1 1 0 1 NJ
+preplace netloc slv_read14_1 1 0 2 NJ 460 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg12 1 2 1 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg3 1 2 1 NJ
+preplace netloc IBUF_DS_P_1 1 0 1 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg13 1 2 1 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg4 1 2 1 NJ
+preplace netloc proc_sys_reset_0_peripheral_aresetn 1 1 2 280 90 NJ
+preplace netloc axi_pcie3_0_axi_aclk 1 1 2 300 110 NJ
+preplace netloc slv_read13_1 1 0 2 NJ 440 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg14 1 2 1 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg5 1 2 1 NJ
+preplace netloc slv_read9_1 1 0 2 NJ 360 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg15 1 2 1 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg6 1 2 1 NJ
+preplace netloc slv_read7_1 1 0 2 NJ 320 NJ
+preplace netloc slv_read1_1 1 0 2 NJ 200 NJ
+preplace netloc slv_read12_1 1 0 2 NJ 420 NJ
+preplace netloc reset_rtl_1 1 0 1 NJ
+preplace netloc regfilex16_v1_0_0_slv_reg7 1 2 1 NJ
+preplace netloc aux_reset_in_1 1 0 1 NJ
+levelinfo -pg 1 0 150 440 600 -top 0 -bot 570
+",
+}
 
   # Restore current instance
   current_bd_instance $oldCurInst
@@ -349,4 +466,6 @@ proc create_root_design { parentCell } {
 
 create_root_design ""
 
+
+puts "\n\nWARNING: This Tcl script was generated from a block design that has not been validated. It is possible that design <$design_name> may result in errors during validation."
 
